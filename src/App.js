02ce2771432d512455ce4344ed85015483f9c1e6
Patrick <patrickmarin@gmail.com>
2020-03-04 13:56:40 -0500

    Fixed Image Sequence

--- src/App.js
+++ src/App.js
@@ -4,36 +4,15 @@ import LetterButton from './LetterButton'
 import Blank from "./Blank"
 import {category, secret} from "./randomWords.js"
 import Background from "./Background"
+import {pictures} from "./Images.js"
 
-//import MatchingBlanks from "./MatchingBlanks"
-const zero = "./images/0.jpg"
-const one = "./images/1.jpg"
-const two = "./images/2.jpg"
-const three = "./images/3.jpg"
-const four = "./images/4.jpg"
-const five = "./images/5.jpg"
-const six = "./images/6.jpg"
-const seven = "./images/7.jpg"
 
 function App() {
   let letters = secret.map(
     (letter) => ({letter: letter, show: false})
   )
   const [lettersObjects, setLetterObjects] = useState(letters)
-
-  function checkLetter(letter){
-    let letra = letter.toLowerCase()
-    let letras = lettersObjects.slice()
-      if (secret.includes(letra)){
-        setLetterObjects(letras.map(
-            (ob) => letra === ob.letter ? {letter: letra, show: true} : ob
-          )
-        )
-      } else {
-        console.log (2)
-      }
-  }
-
+  const [count, setCount] = useState(0)
   const row1Letters = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"].map(
     (letter) => <LetterButton letter={letter} checkLetter={() => checkLetter(letter)} />
   )
@@ -44,10 +23,32 @@ function App() {
     (ob, i) => <Blank key={i} secret={ob.letter} showMe={ob.show} />
   )
 
+  function increment() {
+    setCount( count + 1)
+  }
+  function checkLetter(letter){
+    let letra = letter.toLowerCase()
+    let letras = lettersObjects.slice()
+      if (secret.includes(letra)){
+        setLetterObjects(letras.map(
+            function(ob) {
+              if (letra == ob.letter) {
+                  return {letter: letra, show: true}
+              }else {
+                return ob
+              }
+            }
+          )
+        )
+      } else{
+        increment()
+      }
+  }
+    
   return (
     <div className="App">
       <Background />
-      <img className="hang" src={zero}/>
+      <img className="hang" src= {pictures[count]}/>
       <p></p> 
       {row1Letters}
       <p></p>
