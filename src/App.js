f15149f23353d9db2cdfa1ddff24e197d5430912
Gustavo Galeazzi <gustavogaleazzi@gmail.com>
2020-03-05 14:21:25 -0500

    ducks

--- src/App.js
+++ src/App.js
@@ -53,12 +53,32 @@ function App() {
       <p></p>
       {row2Letters}
       <p></p>
-      <span>The category is: </span>
-      <span>{category}</span>
+
+      <div className="main-div">
+       <div className="category1">T</div>
+       <div className="category2">h</div>
+       <div className="category3">e</div>
+       <div className="category4">_</div>
+       <div className="category2">C</div>
+       <div className="category1">a</div>
+       <div className="category3">T</div>
+       <div className="category2">e</div>
+       <div className="category3">g</div>
+       <div className="category1">o</div>
+       <div className="category2">r</div>
+       <div className="category3">y</div>
+       <div className="category4">_</div>
+       <div className="category3">I</div>
+       <div className="category1">s</div>
+       <div className="category2">:</div>
+       <div className="category1"> </div>
+       {category}
+       </div>
+
       <div className="blank-list">
         {blanks}
       </div>
-
+      
     </div>
   );
   }
